4ffa0d422942b4689ebbda74a86fdc13a2244c60
0315311001-ops <0315311001@s.tusd1.org>
2026-04-24 15:45:31 +0000

    day 1

--- script.js
+++ script.js
@@ -38,6 +38,11 @@ let userAnswers = [];
 let index = 0;
 
 function showNextQuestion() {
+    // Check the answers
+    if (index >= questions.length) {
+        checkAnswers();
+        return;
+    }
     // TODO: Get the current question
     let question = questions[index];
     
@@ -79,17 +84,37 @@ function submitAnswer(event) {
 
     // TODO: Use a conditional (if/else-if) to check which option was clicked and push the option text into the userAnswer array
     if (option1.checked) {
-        console.log("Option1 picked")
+        userAnswers.push(question.option1);
     }
+    else if (option2.checked) {
+        userAnswers.push(question.option2);
+    }
+    else if (option3.checked) {
+        userAnswers.push(question.option3);
+    }
+    )
     // TODO: Add one to the question index
 
     // TODO: Load the next question
+    function submitAnswer(event) {
+
+    }
 }
 
 function checkAnswers() {
     // TODO: Iterate through the user answers array and count how many are correct
-
+    let numCorrect = 0;
+    for (let i = 0; i < userAnswers,length; i++) {
+        let userAnswer = userAnswers[i];
+        let question = questions[i];
+        if (userAnswer == question.answer) {
+            numCorrect++;
+        }
+    }
     // TODO: Show an alert to the user with how many they got right out of the total
 
     // TODO: Reset and start over
+    index = 0;
+    userAnswers = [];
+    showNextQuestion();
 }
